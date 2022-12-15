2fe936b3a167117490d647b7831933bf76fe8087
Radhe Shyam Soni <sradhe545@gmail.com>
2022-12-15 12:30:41 +0530

    Added

--- route/product.routes.js
+++ route/product.routes.js
@@ -1,6 +1,27 @@
 const express=require("express")
 const Product=require("../model/product.model")
 const app=express.Router()
+app.get("/",async(req,res)=>{
+   try{
+   let data=await Product.find({})
+    res.status(200).send(data)
+
+   }catch(e){
+      console.log(e)
+      return res.status(500).send(e)
+   }
+})  
+app.delete("/:id",async(req,res)=>{
+   try{
+      
+    let data=await Product.findByIdAndDelete({_id:req.params.id})
+res.status(200).send("Deleteed Successfully")
+
+   }catch(e){
+      console.log(e)
+      return res.status(500).send(e)
+   }
+}) 
 app.post("/",async(req,res)=>{
    try{
    let data=await Product.create(req.body)
